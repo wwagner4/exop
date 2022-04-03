7572d73860dbd658d1a78f782bb7f718bcd65fab
wwagner4 <wwagner4@gmail.com>
2022-04-03 14:08:42 +0200

    setup react

---
+++ src/exop-react/src/components/Main.tsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return (<div>
+            <h1>exop</h1>
+            <p>visualize known exoplanets</p>
+            <p>
+                <a href="#" onClick={() => console.log("update catalog")}>update catalog</a>
+            </p>
+            <p>
+                <a href="#" onClick={() => console.log("create image")}>create image</a>
+            </p>
+        </div>
+    );
+}
+
